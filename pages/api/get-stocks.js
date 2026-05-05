ec1f3ea818e6f9f29bad9504e603d9e1e2b0f5cd
Megeco <megecovac@gmail.com>
2026-05-05 14:29:43 +0530

    Update get-stocks.js

--- pages/api/get-stocks.js
+++ pages/api/get-stocks.js
@@ -1,5 +1,6 @@
 import { db } from "../../lib/db";
 
-export default function handler(req, res) {
-  res.status(200).json(db.getAll());
+export default async function handler(req, res) {
+  const data = await db.getAll();
+  res.status(200).json(data);
 }
