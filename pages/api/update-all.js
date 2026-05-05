eceebfa3a143feceac14d254e50ccc294e9077a5
Megeco <megecovac@gmail.com>
2026-05-05 14:29:29 +0530

    Update update-all.js

--- pages/api/update-all.js
+++ pages/api/update-all.js
@@ -3,12 +3,18 @@ import { computeScores } from "../../lib/scorer";
 import { decisionEngine } from "../../lib/decision";
 
 export default async function handler(req, res) {
-  const stocks = db.getAll();
+  const stocks = await db.getAll();
 
   for (let s of stocks) {
     const scores = await computeScores(s);
     const decision = decisionEngine(scores);
-    db.update(s.name, { ...scores, ...decision });
+
+    await db.upsert({
+      name: s.name,
+      ...scores,
+      ...decision,
+      updated_at: new Date()
+    });
   }
 
   res.status(200).json({ success: true });
