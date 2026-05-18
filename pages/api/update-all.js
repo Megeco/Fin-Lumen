33259aeefc96f32769edd21045013a870c375ee9
Megeco <megecovac@gmail.com>
2026-05-18 23:24:12 +0530

    Update update-all.js

--- pages/api/update-all.js
+++ pages/api/update-all.js
@@ -33,6 +33,7 @@ export default async function handler(req, res) {
     const { data: stocks, error } = await supabase
       .from('stocks')
       .select('*');
+      .order('name', { ascending: true });
 
     if (error) {
       console.error("FETCH ERROR:", error);
